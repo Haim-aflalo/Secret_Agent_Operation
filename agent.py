5960c5b7b305320c8f8021be24be44329a2c12a7
Haim Aflalo <kodkodhaim@gmail.com>
2025-11-03 12:11:23 +0200

    class agent and private field for clearance level

--- agent.py
+++ agent.py
@@ -1,8 +1,17 @@
 class Agent:
 
-    def __init__(self,code_name,clearance_level):
+    def __init__(self,code_name,clearance_level = 0):
         self.code_name = code_name
-        self.clearance_level = clearance_level
+        self._clearance_level = clearance_level
+
+    def get_clearance_level(self):
+        return self._clearance_level
+
+    def set_clearance_level(self,level):
+        if 0 < level < 11:
+            self._clearance_level = level
 
     def report(self):
-        return f"Agent {self.code_name} reporting. Clearance Level: {self.clearance_level}"
+        return f"Agent {self.code_name} reporting. Clearance Level: {self._clearance_level}"
+
+
