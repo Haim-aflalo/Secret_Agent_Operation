95c2223ae884775d658750e80f1f7c797b70a21e
Haim Aflalo <kodkodhaim@gmail.com>
2025-11-03 12:21:12 +0200

    class FieldAgent

---
+++ special_agent.py
@@ -0,0 +1,11 @@
+from agent import Agent
+
+
+class FieldAgent(Agent):
+    def __init__(self, code_name, clearance_level,region):
+        super().__init__(code_name, clearance_level)
+        self.region = region
+
+    def report(self):
+        super().report()
+        return f"Agent {self.code_name} reporting. Clearance Level: {self._clearance_level}. Region: {self.region}"
